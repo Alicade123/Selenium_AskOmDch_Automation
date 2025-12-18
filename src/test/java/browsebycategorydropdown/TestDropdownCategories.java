78d215943952187971173e31be4a6f859e9c722f
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-18 19:14:13 +0200

    EscRefactoring the dropdown of selecting or browsing the products by their category

--- src/test/java/browsebycategorydropdown/TestDropdownCategories.java
+++ src/test/java/browsebycategorydropdown/TestDropdownCategories.java
@@ -3,6 +3,8 @@ package browsebycategorydropdown;
 import base.BaseTests;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
 
 public class TestDropdownCategories extends BaseTests {
     // Option 1: value="accessories" -> Accessories  (3)
@@ -16,67 +18,143 @@ public class TestDropdownCategories extends BaseTests {
     // Option 9: value="womens-shoes" -> Women’s Shoes  (1)
     // Option 10: value="men" -> Men  (7)
     @Test
-    public void testBrowseMenJeansCategory(){
-        String value = "Men’s Jeans  (4)";
+    public void testBrowseMenJeansCategory() {
+        String categoryValue = "mens-jeans";
+        String expectedCategory = "Men’s Jeans";
+
         var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
-    }
-    @Test
-    public void testBrowseMenShirtsCategory(){
-        String value = "Men’s Shirts  (1)";
-        var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
-    }
-    @Test
-    public void testBrowseMenShoesCategory(){
-        String value = "Men’s Shoes  (1)";
-        var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
     }
 
     @Test
-    public void testBrowsePurse_HandBagsCategory(){
-        String value = "Purses And Handbags  (1)";
+    public void testBrowseMenShirtsCategory() {
+        String categoryValue = "mens-shirts";
+        String expectedCategory = "Men’s Shirts";
+
         var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
-    }
-    @Test
-    public void testBrowseWomenJeansCategory(){
-        String value = "Women’s Jeans  (2)";
-        var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
     }
 
     @Test
-    public void testBrowseWomenShirtsCategory(){
-        String value = "Women’s Shirts  (1)";
+    public void testBrowseMenShoesCategory() {
+        String categoryValue = "mens-shoes";
+        String expectedCategory = "Men’s Shoes";
+
         var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
     }
 
     @Test
-    public void testBrowseWomenShoesCategory(){
-        String value = "Women’s Shoes  (1)";
+    public void testBrowsePurse_HandBagsCategory() {
+        String categoryValue = "purses-and-handbags";
+        String expectedCategory = "Purses And Handbags";
+
         var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
     }
+
     @Test
-    public void testBrowseMenCategory(){
-        String value = "men";
+    public void testBrowseWomenJeansCategory() {
+        String categoryValue = "womens-jeans";
+        String expectedCategory = "Women’s Jeans";
+
         var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
     }
+
     @Test
-    public void testBrowseWomenCategory(){
-        String value = "Women  (7)";
+    public void testBrowseWomenShirtsCategory() {
+        String categoryValue = "womens-shirts";
+        String expectedCategory = "Women’s Shirts";
+
         var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
     }
+
     @Test
-    public void testBrowseAccessoriesCategory(){
-        String value = "Accessories  (3)";
+    public void testBrowseWomenShoesCategory() {
+        String categoryValue = "womens-shoes";
+        String expectedCategory = "Women’s Shoes";
+
         var storePage = homePage.clickStore();
-        storePage.browserProductCategory(value);
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
+    }
+
+    @Test
+    public void testBrowseMenCategory() {
+        String categoryValue = "men";
+        String expectedCategory = "Men";
+
+        var storePage = homePage.clickStore();
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
+    }
+
+    @Test
+    public void testBrowseWomenCategory() {
+        String categoryValue = "women";
+        String expectedCategory = "Women";
+
+        var storePage = homePage.clickStore();
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
+    }
+
+    @Test
+    public void testBrowseAccessoriesCategory() {
+        String categoryValue = "accessories";
+        String expectedCategory = "Accessories";
+
+        var storePage = homePage.clickStore();
+        var chosenProductCategoryPage =
+                storePage.browserProductCategory(categoryValue);
+
+        String actualResult = chosenProductCategoryPage.getProductsCategory();
+        assertTrue(actualResult.contains(expectedCategory),
+                "Expected category to contain: " + expectedCategory);
     }
 
 }
