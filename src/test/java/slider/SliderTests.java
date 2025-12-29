6b1c9e38619bbeb4d2aee6942ecc27b6371640e1
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-12-29 21:11:18 +0200

    The changes added in slider to improve the test

--- src/test/java/slider/SliderTests.java
+++ src/test/java/slider/SliderTests.java
@@ -11,12 +11,12 @@ public class SliderTests extends BaseTests {
         var storePage = homePage.goToStore();
         int [] priceRange = storePage.slider(50, -50);
         System.out.println(priceRange[0]+" - "+priceRange[1]);
-        assertTrue(priceRange[0]<=priceRange[1],"Alicade 🫵🏿 you messed up all the things 💀");
+        assertTrue(priceRange[0]<=priceRange[1],"Alicade 🫵🏿 you messed up all things 💀");
     }
 
     @Test
     public  void priceSliderTestJS(){
         var storePage = homePage.goToStore();
-        assertTrue(storePage.sliderJs(100,10),"Alicade 🫵🏿 you messed up all the things 💀");
+        assertTrue(storePage.sliderJs(100,10),"Alicade 🫵🏿 you messed up all things 💀");
     }
 }
